d7a76629a1ef5cbe4a311adc1b0dc0dd36aea972
dezcor <descorjm@gmail.com>
2017-10-10 14:02:12 -0500

    Ya existe la base logica del server

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-console.log('Hola mundo!','Fuck yeah!');
+const express = require('express')
+const app = express()
 
-var nombre="Jose Migue";
-var nick="dezcorjm";
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-console.log(nombre,'es',nick);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
